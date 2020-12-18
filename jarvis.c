f4f31abc0014d2d7fa180bea22996c83057a17e1
ssprogramer <76197072+ssprogramer@users.noreply.github.com>
2020-12-18 11:28:31 +0530

    jarvis.c

---
+++ jarvis.c
@@ -0,0 +1,213 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include<windows.h>
+#include<time.h>
+void main()
+{
+
+
+    //part of declartions
+    char charcommand[1000];
+    char p[1000]="write your command here ,sir";
+    char h[100]="hello ,sir how may I help you";
+    char y[100]="I can't get your input ,sir";
+    char i[100]="system working properly ,sir";
+    char o[100]="sir here is the current date & time";
+    char i2[1000] ="I am jarvis version 1.0 running on your pc sir";
+    char i3[10000] = "well this is version 1.0 so I can only chat with you , but in later version you will find improvement";
+    char b[10000] = "as your wish sir bye, and have a nice day sir";
+    char n[100] ="sir you name is";
+    char w[1000] ="sir here you can set your name";
+    char s2[100] = "here you go ,sir";
+    char w2[100] ="I am jarvis version 1.0 made by Siddhaant kumaar";
+    char angry[10000] = "are you mad","duffer","are you duffer","you are stupid","are you stupid","fuck off";
+    char ja[1000] = "sir mind you language";
+    char s[1000];
+    char q[1000];
+    char v10[100];
+     time_t t;
+     time(&t);
+
+    //program starts here
+    //set-up
+    printf("Enter your name here sir\n");
+    gets(q);
+    printf("saved succesfully");
+    sleep(2);
+    system("cls");
+    goto xy;
+    //main program
+    strcpy(charcommand,"espeak \"");
+    strcat(charcommand,p);
+    strcat(charcommand,"\"");
+    system(charcommand);
+    xy:
+printf("\n\t\t\t\t\t\t\t\t    mNNdhNNNNNddmdhysooo++oo+++++++/++///////+////+++ooosyhhmmmmmhdmmmmmmmmdyydho//ymmmm            ");
+printf("\n\t\t\t\t\t\t\t\tmmmh+oy+sddddddNNNmNmhmNmdhyssoo+++++//+/////////+//////++///////+//+++osydmhmmmmmmhmmmmy/:-../ohdmm");
+printf("\n\t\t\t\t\t\t\t\tmhoohmmdmddmmNmhmNNNNmdhsooo+oo++//////+++/++++++o+++++++//+////+///////++osydmmmhhmmmmmmdo::/:/osdm");
+printf("\n\t\t\t\t\t\t\t\ts+ydmmmmmmmNNNNmddmmhsso+++++/+++//++oooosyyyyyyo+yhyyyoossso++++///////++//+oshddmmmmmddmmdo:oo/osh");
+printf("\n\t\t\t\t\t\t\t\todmmmmmmmmhdmNNNmhysoooo++////+oossyyyyy+shyo+++ooyhhhhsshhhhysosoo+///++//////+oydmmdhdmmmmdyosy++s");
+printf("\n\t\t\t\t\t\t\t\tmmdmmmmmmNmdhmmdyoo++++++++++osyo+yyyhhhyddhhyyyyyhyhhdddddddhsshhhysoo+////////++oyddmmmmmmdmdsohs/");
+printf("\n\t\t\t\t\t\t\t\tmmmNNmdmmmmmmhysoo+++///+osyyyyyyyhyhyss+o+ooooooooooooooooss/:/oshhddhyo++////++//+ohmmmdhhdmmmyoyy");
+printf("\n\t\t\t\t\t\t\t\tmmmmNmmdhdmdhsoooo+///+osyyyyyhhhyo+++osyhddmmdmddddddddhhyso/:---:+shdddhso+++/////+oyddhdmmmmmmdos");
+printf("\n\t\t\t\t\t\t\t\tmNmNNNNmdhhsooo++++++oyyyyysosso++oyddmmmmmmmmmmmmmmmmmmmmmmmdhyo/---:+shdddyo+///////+sdmmmmmmddmdo");
+printf("\n\t\t\t\t\t\t\t\tNNddmdmmdhoo+++//++oooyyyo/--:/shmmmmmmmmNmmNNNNNNmNNNNNNNNNNNNmmdhs/---/oyhddyo+/////++ohmmdhddmmmd");
+printf("\n\t\t\t\t\t\t\t\tNmmmdhhdyooo+///++o++sso:--:+ydddmmmmNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNmmho:--:oyhdhs++++++/+oyddmmmmmmm");
+printf("\n\t\t\t\t\t\t\t\tmmmmmmdsooo++//+oo/oss/--:+yddddmmNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNmho:--/shhdho++/+++++ydmmmdddd");
+printf("\n\t\t\t\t\t\t\t\tdddmmdsoo++++++o++syo:--/ydddmmmNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNmh+---+yhdds++++//++ydmhddmm");
+printf("\n\t\t\t\t\t\t\t\tmmdddyooo+//++o+osy+:osyhdddmmNNNNNNNNNNNNNNNNNNNNmNNNNNNNNNNNNNNNNNNNNNNNmy/--/yhddso++++++oydmmmmm");
+printf("\n\t\t\t\t\t\t\t\tmmmdhoso+++++o/oyy+/ymddddmmmNNNNNNNNNNNNNNNNmNNNmmNNNNmNNNNNNNNNNNNNNNNNNNNd+--:yhddyo++++++ohmmmmd");
+printf("\n\t\t\t\t\t\t\t\tddmdsooo++++o++yy+/smddmmNNmNNNNNNNNNNNNmNmmmmNmmNmmmmmmNNNNNNNNNNNNNNNNNmmNNd+--/yhddso++++++shmddm");
+printf("\n\t\t\t\t\t\t\t\tmddyso+++++oo+sy+:odddmmNmmmmNNNNNNNNNmmmmmmmmmmmmmmmmmmmmmmmNmNNNNNNNNmmmmmNNd+--/yhddso++++++sdmmm");
+printf("\n\t\t\t\t\t\t\t\tmdhsoo+++++s/sys:/hddmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmd/--ohddys++++++ohdmm");
+printf("\n\t\t\t\t\t\t\t\tddysso++++o++yy/:/yhdsshmmmmmmhssydmmdmmdsssssssyddyssdmmmdysydhssmmmmhysssssymms:-:shddsoo++++oyhdd");
+printf("\n\t\t\t\t\t\t\t\tddssso++++s/syo:-:yhd. +ddddd+`-- :hddddh` /sso` /dh: :hdd/`-yd+ `hddh` :ooosymmd/::oyddyso++++oshmm");
+printf("\n\t\t\t\t\t\t\t\tdhsso++++oo+yh+::+hhh. /dddd: `/+` .ydddh` /-``:+yddd+`.s:`/hdd+ `hddds+/+/``/mmmy/:/yhdhsoo+++ooydm");
+printf("\n\t\t\t\t\t\t\t\tdhsso+++ooooyy//yhh+/-:shoh/-/yyyyo-:hysh--sds:-ohhshdy:--oddsho--hsyy/////-:sdshmd//syddsso++++oshd");
+printf("\n\t\t\t\t\t\t\t\tmhssoo++os+oyy//hhddddddddddhhddddddyhhhhyyoyydhydymhhyshyydydyyhdddddhhhhhdmmmmmmm+/syddssoo+++oohm");
+printf("\n\t\t\t\t\t\t\t\tmddhyyyyyy+oyy//shdmmmmmmddddddddddm+ss+hom+d+d++hsmyhyydo+y/sydydmddddddddmmmmmmmmo/yyddyssooooooyh");
+printf("\n\t\t\t\t\t\t\t\tmmmmddddhhooyy/:/sdmmmmmmmmdmmmmmmmmhddhdhmhmhdddhyhddsydhhmhmyyhNmmmmmmmmNNNNNNNNmo+yyddysooooooshh");
+printf("\n\t\t\t\t\t\t\t\tmmmmmmmmdy+oyy+::+dmmmmmmmmmmNNNmmmmmmmmNNNNmNNNNNNNNmNNNNNNNmmNNNmmNNNNNNNNNNNNNNm++hhdhyysoo+oosyh");
+printf("\n\t\t\t\t\t\t\t\tmmmmmmmmhoyohys/:/ydmmmmmmNmmmNNNNmmmmmmmNNNNmNNNNNNNmNNNNNmmmmNNNNNNNNNNNNNNNNNNNh+shhdhyysooooosyh");
+printf("\n\t\t\t\t\t\t\t\tdmmmdmmmdohysyy+::ohmmmmmmmmddmmmmNmmmmmmmmmNNNNNNNNNNNNNmNmmNmmNNNNNNNNNNNNNNNNNmo+hhddyyysoooooyhh");
+printf("\n\t\t\t\t\t\t\t\tdmmmmmmmdsyhshhs/:/ydmmmmmmmmmmmmmmmmmmmmmmmmmmNNmmNNNNmmNNNNNNNNNNNNNNNNNNNNNNNNh+shhdhyyssoooosyhd");
+printf("\n\t\t\t\t\t\t\t\tmmmmmmmmddohyyhho/:+hmmmmmmmmmmmmmNmdmmmmmmmmmmmmNmmmNmNmmNmNNNNNNNNNNNNNNNNNNNNdoohhddyyysoooosyhhm");
+printf("\n\t\t\t\t\t\t\t\tdddmmmmmmdyshyyyy+::+hdddddhdddddddmmmmmmmmmmdddddhddddhdddddddmNNNNNNNNNNNNNmmdo+hhhhhyysooooosyhhd");
+printf("\n\t\t\t\t\t\t\t\tddmmmmmmmmdyshyyhy+:/+hmmdmmmddddmmmmmmmmmmmmmmmmmdmmddmmmmmmmmmNNNNNmNNNNNNNmhooyhhhhyyysoooooyyhmm");
+printf("\n\t\t\t\t\t\t\t\tmmmmmmmmmmmdsshsyhy+//+ydmmmmmmmmmmmmmmmmmmmNmmmmmmmmmNNNNNNNmNNNNNNNmmmNNNNmhooyhhdhyyssooooosyyhdm");
+printf("\n\t\t\t\t\t\t\t\tmddddmmmmmmmdysyshhyo///shmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmNNNmmmmmmmNNNms+oyyhhhyyysooooosyyyhhh");
+printf("\n\t\t\t\t\t\t\t\tdddmmmmmmmmmmmdhdddhhs+//+ydmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmNNNmmmNmhoosyyyhhhsssooooossyyhddd");
+printf("\n\t\t\t\t\t\t\t\tmmmmmmmmmmmmmmmmmmmmdhys+//+shdmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmNNNNmmdhsoshhhhhyyhyssooossysyhdddm");
+printf("\n\t\t\t\t\t\t\t\tmmmddddmmmmmmmmmmmmmmmdhyo+//+sydmmmmmmmmmmmmmmmmmmmmmmmmddmmmmNmmmNNdhsoyhhhhhhyyyyssssssyhyyhhhddm");
+printf("\n\t\t\t\t\t\t\t\tmmmddmmmmmmmmmmmmmmmmmmdhhys+///+syddmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmdyssyhddddhyyyyssssssyyhhhhddddmd");
+printf("\n\t\t\t\t\t\t\t\tmmmmmmmmdddmmmmmmmmmmmmdmddhyys+///+osyhddmmmmmdddddmdmmmmmmddddysssyhddddmmmdyyysssssyyhhhhhddddddy");
+printf("\n\t\t\t\t\t\t\t\thmmmmdddhhdddddddddddmmdmmmdddhyyso++++++osdmmmdddmdddddddhhyyssssyhhddmmNNNNmdhhhhyyyhhddmNdhhddhss");
+printf("\n\t\t\t\t\t\t\t\tshdmmdddddddddddddddddddmmmmdddddhhhysso+++ossssssyysssooossyyhhhhhddmNNNNNNNmmmdhdddhhhmNNNdhhhhssd");
+printf("\n\t\t\t\t\t\t\t\t+oydmmmmddddhhdmmmmmddddmdmmmmmmmmmdddhyyysyso++ossyyyyyyyyyhhhdddhmmmNNNNNNNmmmdddddhddddNNdhhysydd");
+printf("\n\t\t\t\t\t\t\t\to+ohdmmmmddhddddddddddddmdmmmmmmdmmmmmmmmddddhhssyhhhhhdddddddmmdhymNNNNNNNNNdhddmdddyhhmmNNdyssyhhh");
+printf("\n\n\t\t\t\t\t\t\t>>>");
+gets(s);
+if(strcmp(s,"hello")==0){
+    strcpy(charcommand,"espeak \"");
+    strcat(charcommand,h);
+    strcat(charcommand,"\"");
+    system(charcommand);
+    system("cls");
+    goto xy;
+}
+else if(strcmp(s,"how are you")==0){
+    strcpy(charcommand,"espeak \"");
+    strcat(charcommand,i);
+    strcat(charcommand,"\"");
+    system(charcommand);
+    system("cls");
+    goto xy;
+}
+else if(strcmp(s,"who made you")==0){
+    strcpy(charcommand,"espeak \"");
+    strcat(charcommand,w2);
+    strcat(charcommand,"\"");
+    system(charcommand);
+    system("cls");
+    goto xy;
+}
+else if(strcmp(s,"play a song")==0){
+    strcpy(charcommand,"espeak \"");
+    strcat(charcommand,s2);
+    strcat(charcommand,"\"");
+    system(charcommand);
+    system("C:\\Users\\Siddhant\\Downloads\\Gadi wala Aaya Ghar Se kachra nikal ft. shakira waka waka.mp3");
+    system("cls");
+    goto xy;
+}
+else if(strcmp(s,"what is current time")==0){
+    strcpy(charcommand,"espeak \"");
+    strcat(charcommand,o);
+    strcat(charcommand,"\"");
+    system(charcommand);
+    printf("\n%s", ctime(&t));
+    sleep(3);
+    system("cls");
+    goto xy;
+}
+else if(strcmp(s,"what is my name")==0){
+    strcpy(charcommand,"espeak \"");
+    strcat(charcommand,n);
+    strcat(charcommand,"\"");
+    system(charcommand);
+    printf("%s",q);
+    sleep(3);
+    system("cls");
+    goto xy;
+}
+else if(strcmp(s,"who are you")==0){
+    strcpy(charcommand,"espeak \"");
+    strcat(charcommand,i2);
+    strcat(charcommand,"\"");
+    system(charcommand);
+    system("cls");
+    goto xy;
+}
+else if(strcmp(s,"what you can do")==0){
+    strcpy(charcommand,"espeak \"");
+    strcat(charcommand,i3);
+    strcat(charcommand,"\"");
+    system(charcommand);
+    system("cls");
+    goto xy;
+}
+else if(strcmp(s,"bye jarvis")==0){
+    strcpy(charcommand,"espeak \"");
+    strcat(charcommand,b);
+    strcat(charcommand,"\"");
+    system(charcommand);
+    system("cls");
+}
+else if(strcmp(s,angrey)==0){
+    strcpy(charcommand,"espeak \"");
+    strcat(charcommand,ja);
+    strcat(charcommand,"\"");
+    system(charcommand);
+    system("cls");
+}
+else{
+    strcpy(charcommand,"espeak \"");
+    strcat(charcommand,y);
+    strcat(charcommand,"\"");
+    system(charcommand);
+    system("cls");
+    goto xy;
+}
+}
+
+
+
+
+//loding bar
+void lod(){
+
+
+    // Initialize char for printing
+    // loading bar
+    char a = 177, b = 222;
+    printf("\n\n\n\n");
+    printf("\n\n\n\n\t\t\t\t\t\t\t\t\t\t"
+        "JARVIS LODING\n\n");
+    printf("\t\t\t\t\t\t\t\t\t\t");
+
+
+
+    // Set the cursor again starting
+    // point of loading bar
+    printf("\r");
+    printf("\t\t\t\t\t\t\t\t\t\t");
+
+    // Print loading bar progress
+    for (int i = 0; i < 20; i++) {
+        printf("%c", b);
+
+        // Sleep for 1 second
+        sleep(1);
+    }
+    system("cls");
+}
